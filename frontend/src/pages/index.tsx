061e1e9da39c57ffcd0bf6a7dd2c5b3f10f7261e
jgalvao <jfelipe16@gmail.com>
2023-08-28 14:51:09 -0300

    created login screen;

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,32 @@
+import Head from "next/head";
+import Image from "next/image";
 
+import { Button } from "../components/ui/Button";
+import { Input } from "../components/ui/Input";
+
+import styles from "../../styles/home.module.scss";
+import logoImg from "../../public/logo.svg";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Sujeito Pizza :)</h1>
-    </div>
-  )
+    <>
+      <Head>
+        <title>DrPizza - faça o login</title>
+      </Head>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Dr Pizza" />
+
+        <div className={styles.login}>
+          <form>
+            <Input placeholder="Digite seu email" type="text" />
+            <Input placeholder="Digite sua senha" type="password" />
+            <Button type="submit" loading={false}>
+              Acessar
+            </Button>
+          </form>
+          <a className={styles.text}>Registra-se</a>
+        </div>
+      </div>
+    </>
+  );
 }
